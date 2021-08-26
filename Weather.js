52bc472f2e28be25a6006ec6f1379ad115527df0
inwookie <31870931+inwookie@users.noreply.github.com>
2021-08-26 18:45:43 +0900

    Update Weather.js
    Added condition of the weather

--- Weather.js
+++ Weather.js
@@ -12,6 +12,24 @@ export default function Weather({ temp }) {
 
 Weather.propTypes = {
   temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf([
+    "Thunderstorm",
+    "Drizzle",
+    "Rain",
+    "Snow",
+    "Atmosphere",
+    "Clear",
+    "Clouds",
+    "Mist",
+    "Smoke",
+    "Haze",
+    "Dust",
+    "Fog",
+    "Sand",
+    "Ash",
+    "Squall",
+    "Tornado",
+  ]).isRequired,
 };
 
 const styles = StyleSheet.create({
