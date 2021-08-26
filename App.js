6692c221278fd4e785ca9512f6e87aaab962ae8b
inwookie <31870931+inwookie@users.noreply.github.com>
2021-08-26 18:23:34 +0900

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
 const API_KEY = "30f126da3db004b030095f4585eed3ab";
 
@@ -15,7 +16,7 @@ export default class extends React.Component {
     const { data } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    console.log(data);
+    this.setState({ isLoading: false, temp: data.main.temp });
   };
   getLocation = async () => {
     try {
@@ -24,8 +25,6 @@ export default class extends React.Component {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
-      console.log(coords.latitude, coords.longitude);
     } catch (error) {
       Alert.alert("Can't find you.", "😭");
     }
@@ -34,7 +33,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
   }
 }
